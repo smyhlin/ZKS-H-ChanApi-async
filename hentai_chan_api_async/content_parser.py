1cb014f3ab07344479ee664e78b13ab1468cd9bc
JKearnsl <76239707+JKearnsl@users.noreply.github.com>
2022-02-22 20:47:35 +0300

    Now method images() is not a "property" type

--- hentai_chan_api_async/content_parser.py
+++ hentai_chan_api_async/content_parser.py
@@ -1,15 +1,16 @@
 import json
 import re
 
+from aiograph import Telegraph
+
 
 class MangaContent:
-    def __init__(self, get_site_content_method, manga_url: str):
+    def __init__(self, get_site_content_method, manga):
         self.__get_site_content = get_site_content_method
-        self._url = manga_url
+        self._Manga = manga
 
-    @property
     async def images(self) -> list[str]:
-        soup_content = await self.__get_site_content(self._url, params={'development_access': 'true'})
+        soup_content = await self.__get_site_content(self._Manga.original_url, params={'development_access': 'true'})
         raw_js = soup_content.find_all('script')[2].text
 
         var_data = raw_js.replace('createGallery(data)', '').replace('    ', '').replace('\n', '').replace("'", '"')
@@ -21,3 +22,28 @@ class MangaContent:
             return obj['fullimg']
         else:
             return []
+
+    async def as_telegraph(self,
+                           short_name: str = 'HentaiChan',
+                           author_name: str = 'JKearnsl',
+                           author_url: str = 'https://t.co/JKearnsl',
+                           title: str = None,
+                           description: str = None,
+                           token: str = None
+                           ) -> str:
+        telegraph = Telegraph(token=token)
+
+        images = await self.images()
+        html_manga_content = [f'<img src="{image}"/>' for image in images]
+        description_content = '' if not description else description
+
+        await telegraph.create_account(short_name, author_name, author_url)
+        page = await telegraph.create_page(
+            title=(self._Manga.title if not title else title)[:256],
+            content=f'{description_content}\n{"".join(html_manga_content)}',
+            author_name=author_name,
+            author_url=author_url,
+            access_token=token
+        )
+        await telegraph.close()
+        return page.url
